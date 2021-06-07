dcfcb811ef562681e32df09c2d74a60130c60865
Mahesh Gaikwad <maheshgaikwad8892@gmail.com>
2021-06-07 21:10:03 +0530

    added crud functionality

--- crud/urls.py
+++ crud/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
-from django.urls import path , include
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("about", views.about , name="about")
-    
+    path("about", views.about, name="about"),
+    path("crud", views.crud, name="crud"),
+    path("deletestud/<int:id>", views.crudDelete, name="sdelete"),
+
 ]
--- crud/views.py
+++ crud/views.py
@@ -1,8 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from crud.models import Student
 
 
 def index(request):
     return render(request, "index.html")
 
+
 def about(request):
     return render(request, "about.html")
+
+
+def crud(request):
+    if request.method == "POST":
+        student = Student()
+        if request.POST.get("name") != "" and request.POST.get("age") != "" and request.POST.get("gender") != "SELECT":
+            student.name = request.POST.get("name")
+            student.age = request.POST.get("age")
+            student.gender = request.POST.get("gender")
+            student.save()
+    data = Student.objects.all()
+    return render(request, "crud.html", {"datas": data})
+
+
+def crudDelete(request, id):
+    student = Student.objects.get(id=id)
+    student.delete()
+    return redirect("crud")
